205935dde9a20a7b39d4d6c7f8e2630fea62d494
admin <admin@DESKTOP-VMPGB21>
2024-05-01 16:21:29 +0200

    Add project files.

---
+++ Nongwe.Services.CouponAPI/Controllers/CouponAPI.Controller.cs
@@ -0,0 +1,126 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Nongwe.Services.CouponAPI.Data;
+using Nongwe.Services.CouponAPI.Models;
+using Nongwe.Services.CouponAPI.Models.Dto;
+
+namespace Nongwe.Services.CouponAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CouponAPI : ControllerBase
+    {
+        private readonly ApplicationDbContext _db;
+        private ResponseDto _response;
+        private IMapper _mapper;
+        public CouponAPI(ApplicationDbContext db, ResponseDto response, IMapper mapper)
+        {
+            _db = db;
+            _response = new ResponseDto();
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public ResponseDto GetCoupons()
+        {
+            try
+            {
+                IEnumerable<Coupon> obj = _db.Coupons.ToList();
+                _response.Result = _mapper.Map<IEnumerable<CouponDto>>(obj);
+            }
+
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+
+            return _response;
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public ResponseDto GetCouponById(int id)
+        {
+            try
+            {
+                Coupon objList = _db.Coupons.First(c => c.CouponId == id);
+                _response.Result = _mapper.Map<CouponDto>(objList);
+            }
+
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+
+            return _response;
+        }
+
+        [HttpGet]
+        [Route("GetCouponByCode/{code}")]
+        public ResponseDto GetCouponByCode(string code)
+        {
+            try
+            {
+                Coupon couponCode = _db.Coupons.FirstOrDefault(u => u.CouponCode.ToLower() == code.ToLower());
+                if (couponCode == null)
+                {
+                    _response.IsSuccess = false;
+                }
+                _response.Result = _mapper.Map<CouponDto>(couponCode);
+            }
+
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+
+            return _response;
+        }
+
+        [HttpPost]
+        public ResponseDto CreateCoupon([FromBody] CouponDto couponDto)
+        {
+            try
+            {
+                Coupon coupon = _mapper.Map<Coupon>(couponDto);
+                _db.Coupons.Add(coupon);
+                _db.SaveChanges();
+               
+                _response.Result = _mapper.Map<CouponDto>(coupon);
+            }
+
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+
+            return _response;
+        }
+
+        [HttpPut]
+        public ResponseDto UpdateCoupon([FromBody] CouponDto couponDto)
+        {
+            try
+            {
+                Coupon coupon = _mapper.Map<Coupon>(couponDto);
+                _db.Coupons.Update(coupon);
+                _db.SaveChanges();
+
+                _response.Result = _mapper.Map<CouponDto>(coupon);
+            }
+
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+
+            return _response;
+        }
+    }
+}
---
+++ Nongwe.Services.CouponAPI/MappingCondig.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Nongwe.Services.CouponAPI.Models;
+using Nongwe.Services.CouponAPI.Models.Dto;
+
+namespace Nongwe.Services.CouponAPI
+{
+    public class MappingCondig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config =>
+            {
+                config.CreateMap<CouponDto, Coupon>();
+                config.CreateMap<Coupon, CouponDto>();
+            });
+            return mappingConfig;
+        }
+    }
+}
